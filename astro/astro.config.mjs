c30da54a424156d458fdb3980743672fd27cb27b
Hafsah <chatter@wetcloudmedia.com>
2026-02-10 11:45:18 -0500

    Add Vercel adapter

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,12 +3,15 @@ import { defineConfig } from 'astro/config';
 import { fileURLToPath } from 'node:url';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 import keystatic from '@keystatic/astro';
 
 const workspaceRoot = fileURLToPath(new URL('../../../', import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: vercel(),
 	integrations: [mdx(), react(), keystatic()],
 	vite: {
 		server: {
